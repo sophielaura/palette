470a0be41427bdfec45d1051c5a753260c9b5c7a
User23 <user23@user23de-imac.local>
2018-11-06 15:38:10 +0800

    Initial Commit

---
+++ palette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  palette
+//
+//  Created by User23 on 2018/11/6.
+//  Copyright © 2018 User23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
